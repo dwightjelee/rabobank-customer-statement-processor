2977c3c6f082254e348c49f4c49ce87fee026b92
Dwight Demmers <dwightjelee@users.noreply.github.com>
2021-09-14 09:21:57 +0200

    RB-06: validator service (#11)
    * RB-06: add initial TransactionValidatorService
    
    * RB-06: add validation for unique references and correct mutations

--- customer-statement-processor/src/app/app.component.ts
+++ customer-statement-processor/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CsvParserService} from "./services/csv-parser.service";
 import {CsvDataInterface} from "./models/csv-data.interface";
+import {TransactionValidatorService} from "./services/transaction-validator.service";
+import {TransactionInterface} from "./models/transaction.interface";
 
 @Component({
     selector: 'app-root',
@@ -9,18 +11,27 @@ import {CsvDataInterface} from "./models/csv-data.interface";
 })
 export class AppComponent implements OnInit {
     public title = 'customer-statement-processor';
-    public datasource: any | undefined;
+
     public headers!: string[];
+    public validTransactions: TransactionInterface[];
+    public duplicateTransactions: TransactionInterface[];
+    public incorrectMutations: TransactionInterface[];
 
     constructor(
-        private csvParserService: CsvParserService
+        private csvParserService: CsvParserService,
+        private transactionValidatorService: TransactionValidatorService
     ) {}
 
     ngOnInit() {
         this.csvParserService.loadCsv().subscribe((csvData: string) => {
             const parsedData: CsvDataInterface = this.csvParserService.parseCsvData(csvData);
-            this.datasource = parsedData.data;
             this.headers = parsedData.headers;
+
+            this.transactionValidatorService.validate(parsedData.data).subscribe((data) => {
+                this.validTransactions = data.validTransactions;
+                this.duplicateTransactions = data.duplicateTransActions;
+                this.incorrectMutations = data.incorrectMutations;
+            });
         });
     }
 }
---
+++ customer-statement-processor/src/app/models/transaction.interface.ts
@@ -0,0 +1,8 @@
+export interface TransactionInterface {
+    'Account Number': string;
+    Description: string;
+    'End Balance': string;
+    Mutation: string;
+    Reference: string;
+    'Start Balance': string;
+}
